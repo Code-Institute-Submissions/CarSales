8a8bc8e8b2ae5b886bb1a795236edae1fee9f352
James Horan <horan5034@gmail.com>
2017-09-28 20:51:56 +0100

    Changes to manage.py file part 2

--- manage.py
+++ manage.py
@@ -1,14 +1,18 @@
 # -*- coding: utf-8 -*-
-from car_sales import create_app, db
+from car_sales import create_app
+from car_sales.model import db
 from flask_script import Manager, prompt_bool
 from flask_migrate import Migrate, MigrateCommand
 from car_sales.model import CarSale, Makes, Models, UsedStock, Users
 import car_models as models
 import used_stock as cars
 
-manager = Manager(create_app())
-migrate = Migrate(create_app(), db)
+app = create_app()
+db.init_app(app)
 
+migrate = Migrate(app, db)
+
+manager = Manager(app)
 manager.add_command('db', MigrateCommand)
 
 
@@ -36,6 +40,7 @@ def dropdb():
     if prompt_bool("Are you sure you want to lose all your data"):
         db.drop_all()
         print ('Dropped the database')
+
 
 if __name__ == '__main__':
     manager.run()
