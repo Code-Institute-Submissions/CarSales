6eec560c9d084316f25d265433dae8847b840ab6
James Horan <horan5034@gmail.com>
2017-09-28 20:07:11 +0100

    Using manage.py to build db

--- manage.py
+++ manage.py
@@ -2,6 +2,7 @@
 from car_sales import create_app, db
 from flask_script import Manager, prompt_bool
 from flask_migrate import Migrate, MigrateCommand
+from car_sales.model import CarSale, Makes, Models, UsedStock, Users,
 import car_models as models
 import used_stock as cars
 
@@ -13,19 +14,21 @@ manager.add_command('db', MigrateCommand)
 
 @manager.command
 def insert_data():
+    db.create_all()
+
     #user = Users(first_name="James", last_name="Horan", email="horan5034@gmail.com", password="P@ssw0rd", is_active=True)
 
     #db.session.add(user)
 
-    for car in cars.cars:
-        db.session.add(car)
-    # for make in makes.makes:
-    #     db.session.add(make)
-    # for model in models.models:
-    #     db.session.add(model)
-    # for car in cars:
+    # for car in cars.cars:
     #     db.session.add(car)
-    db.session.commit()
+    # # for make in makes.makes:
+    # #     db.session.add(make)
+    # # for model in models.models:
+    # #     db.session.add(model)
+    # # for car in cars:
+    # #     db.session.add(car)
+    # db.session.commit()
 
 
 @manager.command
